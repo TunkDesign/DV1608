00dc142da13fc128daaaad9fe1d54b2c34b90d0f
Karl Håkansson <webmaster@tunkdesign.com>
2025-04-09 20:23:31 +0200

    Added the base class.

--- src/Card/Card.php
+++ src/Card/Card.php
@@ -4,5 +4,27 @@ namespace App\Card;
 
 class Card
 {
+    protected int $value;
+    protected string $suit;
+
+    public function __construct(int $value = 1, string $suit = 'spades')
+    {
+        $this->value = $value;
+        $this->suit = $suit;
+    }
     
+    public function getValue(): int
+    {
+        return $this->value;
+    }
+
+    public function getSuit(): string
+    {
+        return $this->suit;
+    }
+
+    public function getAsString(): string
+    {
+        return "[{$this->suit} {$this->value}]";
+    }
 }
