1aac38b9beae743a8e0a5f8cb56af81b07e66854
Karl Håkansson <webmaster@tunkdesign.com>
2025-04-09 20:24:33 +0200

    Added the main route.

--- src/Controller/CardGameController.php
+++ src/Controller/CardGameController.php
@@ -2,9 +2,21 @@
 
 namespace App\Controller;
 
+use App\Card\Card;
+use App\Card\CardGraphic;
+use App\Card\Deck;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
 class CardGameController extends AbstractController
 {
 
+    #[Route('/card', name: 'card_start')]
+    public function home(): Response
+    {
+        return $this->render('card/home.html.twig');
+    }
 }
