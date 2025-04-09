04ea32f2872613e634c527d5d2e087a7cd7cf54d
Karl Håkansson <webmaster@tunkdesign.com>
2025-04-09 20:22:15 +0200

    Completed structure.

--- src/Card/Deck.php
+++ src/Card/Deck.php
@@ -2,7 +2,64 @@
 
 namespace App\Card;
 
-class Deck
+class Deck implements \JsonSerializable
 {
+    private array $cards = [];
+
+    public function __construct(bool $graphic = false, array $preloaded = [])
+    {
+        if ($preloaded) {
+            foreach ($preloaded as $item) {
+                $this->cards[] = new CardGraphic($item['value'], $item['suit']);
+            }
+            return;
+        }
+
+        $suits = [
+            'spades',
+            'hearts',
+            'diamonds',
+            'clubs'
+        ];
+
+        foreach ($suits as $suit) {
+            for ($i = 1; $i <= 13; $i++) {
+                if ($graphic) {
+                    $card = new CardGraphic($i, $suit);
+                } else {
+                    $card = new Card($i, $suit);
+                }
+
+                $this->cards[] = $card;
+            }
+        }
+    }
+
+    public function getCards(): array
+    {
+        return $this->cards;
+    }
+
+    public function getCardsBySuit(string $suit): array
+    {
+        return array_filter(
+            $this->cards,
+            fn($card) => $card->getSuit() === $suit
+        );
+    }
+
+    public function shuffle(): void
+    {
+        shuffle($this->cards);
+    }
+
+    public function draw(): ?Card
+    {
+        return array_shift($this->cards);
+    }
     
+    public function jsonSerialize(): mixed
+    {
+        return $this->cards;
+    }
 }
