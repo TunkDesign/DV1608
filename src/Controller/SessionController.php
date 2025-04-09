e591964dd61a9e447f5d9c5b3c6adb2b6bf131aa
Karl Håkansson <webmaster@tunkdesign.com>
2025-04-09 20:20:58 +0200

    Added a route to show session data.

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -3,8 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
 class SessionController extends AbstractController
 {
-    
+    #[Route('/session', name: 'session')]
+    public function session(
+        SessionInterface $session
+    ): Response {
+
+        return $this->render('session.html.twig', [
+                'session' => $session->all()
+        ]);
+    }
 }
