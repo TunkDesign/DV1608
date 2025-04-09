5ef0d38a2264b4133bfb9edc835a7117c518f924
Karl Håkansson <webmaster@tunkdesign.com>
2025-04-09 20:23:11 +0200

    Added virual representation of the Card class.

--- src/Card/CardGraphic.php
+++ src/Card/CardGraphic.php
@@ -2,7 +2,44 @@
 
 namespace App\Card;
 
-class CardGraphic extends Card
+class CardGraphic extends Card implements \JsonSerializable
 {
+    private array $representations = [
+        'spades' => [
+            '🂡', '🂢', '🂣', '🂤', '🂥', '🂦', '🂧', '🂨', '🂩', '🂪', '🂫', '🂭', '🂮'
+        ],
+        'hearts' => [
+            '🂱', '🂲', '🂳', '🂴', '🂵', '🂶', '🂷', '🂸', '🂹', '🂺', '🂻', '🂽', '🂾'
+        ],
+        'diamonds' => [
+            '🃁', '🃂', '🃃', '🃄', '🃅', '🃆', '🃇', '🃈', '🃉', '🃊', '🃋', '🃍', '🃎'
+        ],
+        'clubs' => [
+            '🃑', '🃒', '🃓', '🃔', '🃕', '🃖', '🃗', '🃘', '🃙', '🃚', '🃛', '🃝', '🃞'
+        ]
+    ];
+
+    public function __construct(int $value = 1, string $suit = 'spades')
+    {
+        parent::__construct($value, $suit);
+    }
+
+    public function getAsString(): string
+    {
+        return $this->representations[$this->suit][$this->value - 1] ?? '[Invalid card]';
+    }
     
+
+    public function getColor(): string
+    {
+        return in_array($this->suit, ['hearts', 'diamonds']) ? 'red' : 'black';
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        return [
+            'value' => $this->value,
+            'suit' => $this->suit
+        ];
+    }
 }
