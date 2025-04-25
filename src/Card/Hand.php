b3baba79afa38593292650ee7e36a696817471bf
Karl Håkansson <webmaster@tunkdesign.com>
2025-04-25 15:00:46 +0200

    Added Hand functions.

---
+++ src/Card/Hand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Card;
+
+class Hand
+{
+    private array $cards = [];
+
+    public function addCard(CardGraphic $card): void
+    {
+        $this->cards[] = $card;
+    }
+
+    public function getCards(): array
+    {
+        return $this->cards;
+    }
+
+    public function getValue(): int
+    {
+        $sum = 0;
+        
+        foreach($this->cards as $card) {
+            $sum += $card->getValue();
+        }
+
+        return $sum;
+    }
+}
